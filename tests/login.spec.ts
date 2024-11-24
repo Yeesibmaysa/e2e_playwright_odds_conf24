2c8330d9f2d4633bdac745d55890c96d7a1954b7
Yeesibmaysa Kanchanasutha <yeesibmaysa@Yeesibmaysas-Laptop.local>
2024-11-24 23:03:56 +0700

    Remove comments login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,8 +13,6 @@ test('Test Login Success', async ({ page }) => {
 
 test('Test Login Fail from not input username and password', async ({ page }) => {
     await page.goto('https://workshop-playwright.vercel.app/');
-    // await page.getByTestId('username-input').fill('Yeesibmaysa');
-    // await page.getByTestId('password-input').fill('123456');
     await page.getByRole('button', { name: 'Login' }).click();
     await page.getByText('Username is required');
     await page.getByText('Password is required').click();;
@@ -26,10 +24,8 @@ test('Test Login Fail from not input username and password', async ({ page }) =>
 
   test('Test Login Fail case not input username but input password', async ({ page }) => {
     await page.goto('https://workshop-playwright.vercel.app/');
-    // await page.getByTestId('username-input').fill('Yeesibmaysa');
     await page.getByTestId('password-input').fill('123456');
     await page.getByRole('button', { name: 'Login' }).click();
-    // await page.getByText('Username is required');
     await expect (page.getByText('Username is required')).toBeVisible();
 
   });
@@ -37,9 +33,7 @@ test('Test Login Fail from not input username and password', async ({ page }) =>
   test('Test Login Fail case input username but not input password', async ({ page }) => {
     await page.goto('https://workshop-playwright.vercel.app/');
     await page.getByTestId('username-input').fill('Yeesibmaysa');
-    // await page.getByTestId('password-input').fill('123456');
     await page.getByRole('button', { name: 'Login' }).click();
-    // await page.getByText('Username is required');
     await expect (page.getByText('Password is required')).toBeVisible();
 
   });
